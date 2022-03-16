61e7305b31623f703c36ebd7f9c766f3b5256462
SimonVue <viktorov_@mail.ru>
2022-03-16 22:46:54 +0300

    add animation promo__title

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 'use strict'
 
+/* ======================= animation ====================== */
+const animItems = document.querySelectorAll('._anim-items');
+
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - innerHeight / animStart;
+      }
+
+      if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      } else {
+        animItem.classList.remove('_active');
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollY || document.documentElement.scrollLeft,
+      scrollТop = window.scrollY || document.documentElement.scrollТop;
+    return { top: rect.top + scrollТop, left: rect.left + scrollLeft }
+  };
+  animOnScroll();
+}
+
+
 /* ======================= show-header ====================== */
 window.onscroll = function showHeader() {
 
@@ -109,3 +145,6 @@ function locationValidation() {
 
 }
 locationValidation();
+
+
+
